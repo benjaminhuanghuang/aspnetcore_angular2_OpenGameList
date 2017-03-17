188096478567f2c058b166ea5a152e57b799250e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-16 21:04:09 -0700

    Angular2 works

--- OpenGameListWebApp/Scripts/app/app.component.ts
+++ OpenGameListWebApp/Scripts/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 @Component({
-    selector: "opengamelist",
+    selector: "my-app",
     template: `<h1>OpenGameList</h1><div>Work in progress...</div>`
 })
 export class AppComponent { }
